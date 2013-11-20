a0b509893a6837b2a112e2b212d73a4720aeb448
Milenko Jovanovic <mile986@gmail.com>
2013-11-20 14:40:16 +0100

    Updated to use WeatherData instead of JSON, fixes

--- ClearWeather/src/com/chainsaw/clearweather/FetchService.java
+++ ClearWeather/src/com/chainsaw/clearweather/FetchService.java
@@ -1,11 +1,9 @@
 package com.chainsaw.clearweather;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -19,12 +17,12 @@ public class FetchService extends Service {
 		final FetchService context = this;
 		final Intent intent = i;
 		final int widgetId;
-		
+
 		Bundle extras = intent.getExtras();
 		if (extras != null)
 			widgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID);
 		else
-			widgetId=0;
+			widgetId = 0;
 		Log.i("Service:widgetID", String.valueOf(widgetId));
 
 		LocationData loc = new LocationData(context);
@@ -33,26 +31,38 @@ public class FetchService extends Service {
 		WeatherDataListener listener = new WeatherDataListener() {
 			@Override
 			public void onDataReady(WeatherData weatherData) {
-				int temperature = -10;
-				int humidity = -10;
-				String name = "null";
-				
+				String temperature = "n/a";
+				String humidity = "n/a";
+				String name = "n/a";
+
 				AppWidgetManager appWidgetManager = AppWidgetManager
 						.getInstance(context);
 
+				SharedPreferences perfs = FetchService.this
+						.getSharedPreferences("com.chainsaw.clearweather",
+								MODE_PRIVATE);
+				if (perfs.getBoolean("isCelsius", true)) {
+					temperature = String.valueOf(weatherData.getTempC()) + "°C";
+				} else {
+					temperature = String.valueOf(weatherData.getTempF())+ "°F";
+				}
+				
+				humidity=String.valueOf(weatherData.getHumidity())+"%";
+				name = weatherData.getCityName();
+
 				RemoteViews remote = new RemoteViews(context.getPackageName(),
 						R.layout.widget_layout);
-				remote.setTextViewText(R.id.temp, String.valueOf(temperature)+"°C");
-				remote.setTextViewText(R.id.humidity, String.valueOf(humidity)+"%");
+				remote.setTextViewText(R.id.temp, temperature);
+				remote.setTextViewText(R.id.humidity, humidity);
 				remote.setTextViewText(R.id.location, name);
-				
+
 				if (widgetId != 0) {
 					appWidgetManager.updateAppWidget(widgetId, remote);
-					// stopSelf(startId);
+					stopSelf(startId);
 					Log.i("Service",
 							"Should update..." + context.getPackageName()
 									+ " temp:" + String.valueOf(temperature));
-					
+
 				}
 			}
 		};
