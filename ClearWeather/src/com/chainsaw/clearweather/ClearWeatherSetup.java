bd4234e4e287b09073d716eae7c21cfc6a1939b0
Milenko Jovanovic <mile986@gmail.com>
2013-11-23 05:09:47 +0100

    bugfix

--- ClearWeather/src/com/chainsaw/clearweather/ClearWeatherSetup.java
+++ ClearWeather/src/com/chainsaw/clearweather/ClearWeatherSetup.java
@@ -5,7 +5,6 @@ import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
